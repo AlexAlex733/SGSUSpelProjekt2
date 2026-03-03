948ddbed00a4f0f06d470335cad08670f321e817
AlexAlex733 <alexander.phetsai@elev.ga.lbs.se>
2026-03-03 14:52:14 +0100

    Alex commit 8
    Added a script that shows a marker for objects that are interactable.

---
+++ Assets/Script/GUI scripts/Interaction script.cs
@@ -0,0 +1,60 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class Interactionscript : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject showImage;
+    [SerializeField] private GameObject interactionMarker;
+    [SerializeField] public GameObject player;
+
+    [Header("Variables")]
+    [SerializeField] private bool markerExist = false;
+    [SerializeField] private Vector3 offset = new Vector3(75f, 75f, 0);
+
+    private void Start()
+    {
+        
+        showImage.SetActive(false);
+        try
+        {
+            player = FindAnyObjectByType<PlayerMovement>().gameObject;
+            interactionMarker = player.transform.GetChild(0).gameObject;
+        }
+        catch
+        {
+            Debug.Log("Reference has failed");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        MarkerInstantiate(true);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        MarkerInstantiate(false);
+    }
+
+    public void MarkerInstantiate(bool enter)
+    {
+        GameObject marker = null;
+        if (enter)
+        {
+            marker = Instantiate(interactionMarker, this.transform.position + new Vector3(0,1,0), Quaternion.identity);
+            marker.transform.localScale = player.transform.localScale * 1.1f;
+            marker.name = "Current Selection";
+            marker.tag = "MarkerTag";
+        }
+        else if (!enter)
+        {
+            Destroy(GameObject.FindWithTag("MarkerTag"), 0.00001f);
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+}
