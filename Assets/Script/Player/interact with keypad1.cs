4784236695999e6801d0181da2376624750a480d
Agusti087 <oskar@kullgren.org>
2026-03-03 14:17:21 +0100

    fixed some major shit

--- Assets/Script/Player/interact with keypad1.cs
+++ Assets/Script/Player/interact with keypad1.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class interactwithkeypad1 : MonoBehaviour
 {
+    [SerializeField] GameObject Keypad;
+    public bool TurnOffKeypad;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        TurnOffKeypad = false;
+        Keypad.SetActive(TurnOffKeypad);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Keypad.SetActive(TurnOffKeypad);
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            TurnOffKeypad = true;
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            TurnOffKeypad = false;
+        }
     }
 }
