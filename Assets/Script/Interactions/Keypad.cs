b653d47db9f8bad8fdf88b81ec89d540b23705f0
AlexAlex733 <alexander.phetsai@elev.ga.lbs.se>
2026-03-06 13:23:52 +0100

    Alex commit 18
    Worked on adding the keypad.

---
+++ Assets/Script/Interactions/Keypad.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Keypad: MonoBehaviour, IInteract
+{
+    [SerializeField] private GameObject keypad;
+    [SerializeField] private bool isTriggered;
+    [SerializeField] private PlayerMovement playerMovement;
+
+    private void Awake()
+    {
+        keypad = GameObject.FindWithTag("Keypad");
+        playerMovement = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
+        keypad.SetActive(false);
+    }
+    public void OnInteract()
+    {
+        Debug.Log("Is this even working? " + isTriggered);
+
+        isTriggered = !isTriggered;
+
+        if (isTriggered)
+        {
+            keypad.SetActive(true);
+            playerMovement.enabled = false;
+        }
+        if (!isTriggered)
+        {
+            keypad.SetActive(false);
+            playerMovement.enabled = true;
+        }
+    }
+}
