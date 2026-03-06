5c79096bd04eec3e21de694716798ef0f90771d7
gben5692 <gben5692@gmail.com>
2026-03-06 13:21:12 +0100

    Added More Comments and finishing off

--- Assets/Script/_Test/_TestInteract.cs
+++ Assets/Script/_Test/_TestInteract.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 
+// Made by Rami
 public class _TestInteract : MonoBehaviour, IInteract
 {
-    public void OnInteract()
+    public void OnInteract() // this is for testing and to show how other interactables look like
     {
         Debug.Log("Player Interacted");
     }
